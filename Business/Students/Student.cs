62bb45c9fa0443036d4820558d4ada690bea1a4a
Siyamamkela Mafu <siyamamkelam@batsamayi.co.za>
2021-07-21 01:09:46 +0200

    Added CRUD fuctions in Business.Student and StudentBL

---
+++ Business/Students/Student.cs
@@ -0,0 +1,53 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Students
+{
+    public class Student : BaseObject
+    {
+        #region Properties
+        public string FName { get; set; }
+        public string Surname { get; set; }
+        #endregion
+        #region CRUD
+        internal static IQueryable<Student> Read(TonicEntities context)
+        {
+            return (from s in context.Students
+                    select new Student
+                    {
+                        Id = s.Id,
+                        FName = s.FName,
+                        Surname = s.Surname,
+                        Active = s.Active
+                    });
+        }
+        internal Data.Student Create()
+        {
+            return new Data.Student
+            {
+                Id = Id,
+                FName = FName,
+                Surname = Surname
+            };
+        }
+        internal void Update(Data.Student entity)
+        {
+            if (entity.Id != Id)
+                throw new ArgumentException();
+
+            entity.FName = FName;
+            entity.Surname = Surname;
+        }
+        internal void Delete(Data.Student entity)
+        {
+            if (entity.Id != Id)
+                throw new ArgumentException();
+            entity.Active = false;
+        }
+        #endregion
+    }
+}
---
+++ Business/Students/StudentBL.cs
@@ -0,0 +1,85 @@
+﻿using Business.CustomErrorMessages;
+using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Students
+{
+   public class StudentBL
+    {
+        public static long Save(Student model)
+        {
+            using (var context = TonicDTO.Context)
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+                    if (model.IsNew)
+                    {
+                        var entity = model.Create();
+                        context.SaveChanges();
+                        transaction.Commit();
+                        return entity.Id;
+                    }
+                    else
+                    {
+                        var entity = GetEntity(model.Id);
+                        if (entity == null)
+                            CustomErrorMessage.InvalidObject("student");
+
+                        model.Update(entity);
+                        context.SaveChanges();
+                        transaction.Commit();
+                        return entity.Id;
+                    }
+                }
+                catch (ArgumentException exc)
+                {
+                    transaction.Rollback();
+                    throw exc;
+                }
+                catch (Exception exc)
+                {
+                    transaction.Rollback();
+                    throw exc;
+                }
+            }
+        }
+        public static void Delete(long id)
+        {
+            using (var context = TonicDTO.Context)
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+                    var entity = GetEntity(id);
+                    if (entity == null)
+                        CustomErrorMessage.InvalidObject("student");
+
+                    new Student().Delete(entity);
+                    context.SaveChanges();
+                    transaction.Commit();
+                }
+                catch (ArgumentException exc)
+                {
+                    transaction.Rollback();
+                    throw exc;
+                }
+                catch (Exception exc)
+                {
+                    transaction.Rollback();
+                    throw exc;
+                }
+
+            }
+        }
+        private static Data.Student GetEntity(long id)
+        {
+            using (var context = TonicDTO.Context)
+                return context.Students.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
