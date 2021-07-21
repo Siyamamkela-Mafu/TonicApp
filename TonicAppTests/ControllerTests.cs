51e7bf96c5f5f71c20216d30e53832d7a5fcf53a
Siyamamkela Mafu <siyamamkelam@batsamayi.co.za>
2021-07-22 01:35:48 +0200

    Added HomeControllerTest and ImportControllerTest test classes with Index TestMethods

---
+++ TonicAppTests/ControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Web.Mvc;
+using TonicApp;
+using TonicApp.Controllers;
+
+namespace TonicAppTests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void Index()
+        {
+            HomeController controller = new HomeController();
+            ViewResult result=controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ TonicAppTests/ImportControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Web.Mvc;
+using TonicApp;
+using TonicApp.Controllers;
+
+namespace TonicAppTests
+{
+    [TestClass]
+    public class ImportControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {           
+                ImportController controller = new ImportController();
+                ViewResult result = controller.Index() as ViewResult;
+                Assert.IsNotNull(result);            
+        }
+    }
+}
